9d9ec1cacfc98baf74fc2137aec402e296727ea1
Alexander Khrustalev <st.alex.ser@gmail.com>
2021-12-19 17:55:44 +0200

    Добавлена страница registration

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 // Custom JS
 document.addEventListener('DOMContentLoaded', () => {
-	
-	$('.enter__main button.btn').on('click', function() {
+
+	$('.enter-main button.btn').on('click', function() {
 		var $this = $(this);
 		 $this.addClass('loading');
 		setTimeout(function () {
 			 $this.removeClass('loading');
 		}, 2000)
+	});
+
+	$('body').on('click', '.password-control', function(e){
+		if ($('.password input').attr('type') == 'password'){
+			$(this).addClass('view');
+			$(this).closest('.password').find('input').attr('type', 'text');
+		} else {
+			$(this).removeClass('view');
+			$(this).closest('.password').find('input').attr('type', 'password');
+		}
+		return false;
 	});
 
 })
